60221b79967e1b877187d57b094af9429c46581a
Ariwa042 <116925898+Ariwa042@users.noreply.github.com>
2024-05-21 10:23:17 +0100

    Updated CSS for button

--- consignment/admin.py
+++ consignment/admin.py
@@ -6,6 +6,13 @@ class TrackingCodeAdmin(admin.ModelAdmin):
   list_display = ('tracking_code', )
 
 
+class PackageAdmin(admin.ModelAdmin):
+  list_display = ('package_id', 'package_name', 'package_weight',
+                  'mode_of_transit', 'package_status', 'package_date',
+                  'package_location', 'package_destination',
+                   'sender',
+                  'receiver', 'package_description', 'package_quantity', 'pdf')
+
 class CustomerAdmin(admin.ModelAdmin):
   list_display = (
       'customer_name',
@@ -15,21 +22,13 @@ class CustomerAdmin(admin.ModelAdmin):
   )
 
 
-class PackageAdmin(admin.ModelAdmin):
-  list_display = ('package_id', 'package_name', 'package_weight',
-                  'mode_of_transit', 'package_status', 'package_date',
-                  'package_location', 'package_destination',
-                  'sending_location', 'receiving_location', 'sender',
-                  'receiver', 'package_description', 'package_quantity', 'pdf')
-
-
 class ShipAdmin(admin.ModelAdmin):
   list_display = ('sending_location', 'receiving_location')
 
 
+admin.site.register(Package, PackageAdmin)
 admin.site.register(TrackingCode, TrackingCodeAdmin)
 admin.site.register(Customer, CustomerAdmin)
-admin.site.register(Package, PackageAdmin)
 admin.site.register(Ship, ShipAdmin)
 
 # Register your models here.
