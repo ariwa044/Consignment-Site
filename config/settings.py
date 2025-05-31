4e05edaa4a507c3acb86208d7316b61e31ed9c49
Ariwa042 <nwezemichael45@gmail.com>
2025-05-31 17:54:20 +0100

    changed debug to true

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'your-default-secret-key-here')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*', 'https://www.chasexpress.com'
 ]
